989f6f02831af66294e5543b3fe56cb676b1d8b9
Roman Gladkikh <roman.gladkikh@outlook.com>
2017-02-16 02:21:04 +0700

    Проекты лабораторных работ и статическая библиотека.

---
+++ Lab1/main.c
@@ -0,0 +1,5 @@
+#include "SimpleComputer.h"
+
+int main() {
+    return 0;
+}
---
+++ SimpleComputer/SimpleComputer.c
@@ -0,0 +1,16 @@
+#include "SimpleComputer.h"
+
+int SC_MemoryInit();
+
+int SC_MemorySet(int address, int value);
+int SC_MemoryGet(int address, int *value);
+
+int SC_MemorySave(char *fileName);
+int SC_MemoryLoad(char *fileName);
+
+int SC_RegInit();
+int SC_RegSet(int register, int value);
+int SC_RegGet(int register, int *value);
+
+int SC_CommandEncode(int command, int operand, int *value);
+int SC_CommandDecode(int value, int *command, int *operand);
